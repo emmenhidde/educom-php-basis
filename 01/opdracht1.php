9ab9ed8134959434a44be38e5eae26658aa13bb0
emmenhidde <emmenhidde@outlook.com>
2026-09-23 12:24:38 +0200

    Dit is het eerste gebruik van wat visueel moet worden en hoe variableen werken in php

---
+++ 01/opdracht1.php
@@ -0,0 +1,18 @@
+<?php
+// Personal information
+$name = "Hidde";
+$age = 26;
+$place = "Gangelt";
+$hobbies = ["Hyrox", "Voetbal", "Skiën"];
+$moreInfo = "Ik had veel meer moeite met de setup dan het programmeren";
+
+echo "Mijn naam is $name. <br>";
+echo "Ik ben $age jaar en woon in $place. <br><br>";
+
+echo "Mijn hobbies zijn: <br>";
+echo "1. $hobbies[0] <br>";
+echo "2. $hobbies[1] <br>";
+echo "3. $hobbies[2] <br><br>";
+
+echo "Overige informatie: $moreInfo.";
+?>
