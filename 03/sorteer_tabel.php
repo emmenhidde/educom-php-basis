e7470adb28249053149075f3f0fc29deb646b715
emmenhidde <emmenhidde@outlook.com>
2026-09-24 10:44:30 +0200

    Opdr1 visie en table naam-adres

---
+++ 03/sorteer_tabel.php
@@ -0,0 +1,17 @@
+<?php
+$personen = [
+    ["naam" => "Jan de Vries", "adres" => "Kerkstraat 12, Amsterdam"],
+    ["naam" => "Sophie Jansen", "adres" => "Dorpsweg 45, Rotterdam"],
+    ["naam" => "Mark Peters", "adres" => "Stationsstraat 8, Eindhoven"],
+    ["naam" => "Lisa van Dijk", "adres" => "Marktplein 21, Maastricht"],
+    ["naam" => "Tom Smit", "adres" => "Molenweg 17, Utrecht"],
+    ["naam" => "Emma Bakker", "adres" => "Schoolstraat 33, Tilburg"],
+    ["naam" => "Daan Visser", "adres" => "Wilhelminastraat 6, Breda"],
+    ["naam" => "Noa Meijer", "adres" => "Beukenlaan 14, Nijmegen"],
+    ["naam" => "Lucas Bos", "adres" => "Parkweg 28, Arnhem"],
+    ["naam" => "Mila Vos", "adres" => "Lindestraat 9, Sittard"]
+];
+
+
+
+?>
